347d5cbe5d45ea0d776aa261a98d1eeef2282f16
AlenaChet <chetvernina2016@gmail.com>
2023-09-06 12:16:33 +0300

    Change

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,23 +1,23 @@
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, viewsets
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
-from rest_framework.viewsets import ModelViewSet
 
-from advertisements.models import Advertisement
-from advertisements.serializers import AdvertisementSerializer
+from .filters import AdvertisementFilter
+from .models import Advertisement
+from .permissions import IsAuthorOrReadOnly
+from .serializers import AdvertisementSerializer
 
 
-class AdvertisementViewSet(ModelViewSet):
-    """ViewSet для объявлений."""
+class AdvertisementViewSet(viewsets.ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
-
-
     def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update", "destroy"]:
-            permission_classes = [IsAuthenticated]
+        if self.request.method == 'POST':
+            self.permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
         else:
-            permission_classes = [AllowAny]
-
-        return [permission() for permission in permission_classes]
+            self.permission_classes = [permissions.IsAuthenticated]
+        return super().get_permissions()
